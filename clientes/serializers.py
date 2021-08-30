3a0d13a2a6c6474e18de1faebf3d103dbcea53a1
guilherme <guilherme.goes.garcia@hotmail.com>
2021-08-30 14:29:40 -0300

    Initial commit

---
+++ clientes/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from clientes.models import Cliente
+from clientes.validators import *
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+    def validate(self,data):
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':'Não incluir numeros nesse campo'})
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':'O CPF deve ter 11 dígitos'})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':'O Rg deve conter 9 dígitos'})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':'O celular deve ter ao menos 11 dígitos'})
+        return data
+
+
